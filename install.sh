8a4c82112194cb7d1f7eb2ba653b1ecd15410f65
hungpn94 <hungpn94@gmail.com>
2024-02-21 21:08:53 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+random() {
+  tr </dev/urandom -dc A-Za-z0-9 | head -c5
+  echo
+}
+
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+
+gen64() {
+  ip64() {
+    echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+  }
+  echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
+
+install_3proxy() {
+  echo "installing 3proxy"
+  URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+  wget -qO- $URL | bsdtar -xvf-
+  cd 3proxy-3proxy-0.8.6
+  make -f Makefile.Linux
+  mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+  cp src/3proxy /usr/local/etc/3proxy/bin/
+  cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+  chmod +x /etc/init.d/3proxy
+  chkconfig 3proxy on
+  cd $WORKDIR
+}
+
+gen_3proxy() {
+  cat <<EOF
+daemon
+maxconn 1000
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+flush
+auth none
+$(awk -F "/" '{print \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
+}
+
+# Các hàm khác không cần thay đổi
+
+echo "installing apps"
+yum -y install gcc net-tools bsdtar zip >/dev/null
+
+install_3proxy
+
+echo "working folder = /home/proxy-installer"
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd $_
+
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
+
+echo "How many proxy do you want to create? Example 500"
+read COUNT
+
+FIRST_PORT=10000
+LAST_PORT=$(($FIRST_PORT + $COUNT))
+
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+chmod +x boot_*.sh /etc/rc.local
+
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+# Các bước khác không cần thay đổi
+
+bash /etc/rc.local
+
+gen_proxy_file_for_user
+
+# upload_proxy
+
+install_jq && upload_2file
